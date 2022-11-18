d84883aa7587cd0da6f483089f65bd6aaa9e8eb8
luancatarino <luancatarino@gmail.com>
2022-11-18 08:30:47 -0300

    feat: question size

--- components/Statement/styles.js
+++ components/Statement/styles.js
@@ -6,5 +6,9 @@ export const Container = styled.div`
     .text {
         font-size: 2.5rem;
         font-weight: 600;
+
+        @media (min-width: 1280px) {
+            font-size: 1.8rem;
+        }
     }
 `;
